f77eba7275d536f9b057773fad4073c1de891c44
Giselly Rock <gisellydsr@gmail.com>
2023-03-17 10:35:10 -0300

    adding migrations

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index() {
+
+    $nome = "Matheus";
+    $idade = 29;
+
+    $arr = [1, 2, 3,4 , 5];
+
+    return view('welcome',
+    [
+        'nome' => $nome,
+        'idade2' => $idade,
+        'profissao' => "Programador",
+        'arr' => $arr
+    ]);
+
+    }
+
+    public function create() {
+        return view('events.create');
+    }
+}
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,17 +6,50 @@
 
         <title>@yield('title')</title>
 
+         <!-- Fonte do Google -->
+         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/scripts.js"></script>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/logo.ico" alt="HDC Events">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a href="/events/create" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a href="" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                        <a href="" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
    @yield('content')
     <footer>
-        <p>HDC Events &copy; 2020</p>
+        <p>HDC Events &copy; 2023</p>
     </footer>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     </body>
 </html>
